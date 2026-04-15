3bf51409fa7b0722a59ca6918c7e86fa4054e00e
Rogerio Nascimento (Mac) <rnascim@gmail.com>
2026-04-15 02:15:43 +0100

    Minor adjustments: loading

--- init.lua
+++ init.lua
@@ -39,7 +39,11 @@ end)
 
 
 if vim.g.neovide then
-    vim.g.neovide_scale_factor = (vim.g.neovide_scale_factor or 1.0) + 0.3
+  vim.keymap.set({ "n", "v", "i" }, "<D-v>", function()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  vim.opt.clipboard = "unnamedplus"
+  end, { desc = "Paste from system clipboard" })
+  vim.g.neovide_scale_factor = (vim.g.neovide_scale_factor or 1.0) + 0.3
     vim.keymap.set({ "n", "v" }, "<c-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
     vim.keymap.set({ "n", "v" }, "<c-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
     vim.keymap.set({ "n", "v" }, "<c-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
